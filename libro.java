7f3b013b596cfd58066590c44a2364535ca0a946
Jeromarquez9 <jeromarquez9m@gmail.com>
2025-10-29 20:59:46 -0300

    Create libro.java

---
+++ libro.java
@@ -0,0 +1,27 @@
+public class libro {
+ }
+public set getAutor() {
+    return autor;
+}
+public void setAutor(set autor) {
+    this.autor = autor;
+}
+public set getAño() {
+    return Año;
+}
+public void setAño(set año) {
+    set Año = año;
+}
+public set getLibro() {
+    return libro;
+}
+public void setLibro(set libro) {
+    this.libro = libro;
+}
+public String mostrarDatos() {
+    return "Libro: [titulo] – Autor: [autor] – Año: [anioPublicacion]";
+}
+public String tipoPrestamo() {
+    return "Préstamo por 21 días";
+    
+}
