36598a47119bb533b5dfdac4c76ffdbd7fddefca
Jeromarquez9 <jeromarquez9m@gmail.com>
2025-10-29 21:00:37 -0300

    Create publicacion.java

---
+++ publicacion.java
@@ -0,0 +1,24 @@
+public class publicacion {
+     }
+    public set getLibro() {
+    return libro;
+}
+
+public void setLibro(set libro) {
+    this.libro = libro;
+
+    public set getAño() {
+    return Año;
+}
+public void setAño(set año) {
+    set Año = año;
+}
+
+
+public String mostrarDatos() {
+    String titulo;
+    String anioPublicacion;
+    return "Publicación: " + titulo + " – Año: " + anioPublicacion;
+}
+    
+}
