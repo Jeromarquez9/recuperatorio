afd89b6d93267802a6e9fc8e1ea62a5fc69b50e5
Jeromarquez9 <jeromarquez9m@gmail.com>
2025-10-29 21:01:14 -0300

    Create revista.java

---
+++ revista.java
@@ -0,0 +1,27 @@
+public class revista {
+     }
+   public set getEdicionN°() {
+    return getEdicionN;
+}
+public void setgetEdicionN(set getEdicionN) {
+    this.getEdicionN = getEdicionN;
+}
+public set getAño() {
+    return Año;
+}
+public void setAño(set año) {
+    set Año = año;
+}
+public set getLibro() {
+    return libro;
+}
+public void setrevista(set revista) {
+    this.revista = revista;
+public String mostrarDatos() {
+    return "Revista: [titulo] – EdiciónN° [numeroEdicion] – Año: [anioPublicacion]";
+}
+public String tipoPrestamo() {
+    return "Préstamo por 15 días";
+}
+    
+}
